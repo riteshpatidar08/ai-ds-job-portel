15fa8c26bc9716e36963a78200ee0c6128596f89
Ritesh <itsmeriteshpatidar@gmail.com>
2025-03-07 10:32:42 +0530

    added home route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const app = express() ;
 dotenv.config() ;
 
 
+app.get('/' , ()=>{
+    res.send('homepage')
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running ${process.env.PORT}`)
 })
